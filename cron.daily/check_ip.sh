3b9cc58c722fb751a71dbe3bdd9e2f99189c0923
mljdivemaster <mljdivemaster@gmail.com>
2016-06-09 23:38:59 +0200

    Update check_ip.sh

--- cron.daily/check_ip.sh
+++ cron.daily/check_ip.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 date +"%Y-%m-%d: " | tr -d 'n' >> /var/log/external-ip.log
 curl http://ip.thomashunter.name?newline -s >> /var/log/external-ip.log
+curl http://ip.thomashunter.name?newline -s |mail -s "`hostname` ip" root
